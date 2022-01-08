0516a95580b6a1b18c6777b7d165db61081364f5
Leon Si <contact@leonzalion.com>
2022-01-08 12:33:20 -0500

    update options

--- src/eslint/global-rules.ts
+++ src/eslint/global-rules.ts
@@ -1,16 +1,37 @@
 module.exports = {
 	rules: {
-		'import/no-unassigned-import': 'off',
-		'@typescript-eslint/consistent-type-imports': 'error',
-		'node/file-extensions-in-import': 'off',
-		'unicorn/prevent-abbreviations': 'off', // code is sometimes clearer with abbreviations
-		'no-else-return': 'off', // code is sometimes clearer with an else
-		'no-lonely-if': 'off', // code is sometimes clearer with a lonely if
-		'import/extensions': [
-			'error',
+		"import/no-unassigned-import": "off",
+		"@typescript-eslint/consistent-type-imports": "error",
+		"node/file-extensions-in-import": "off",
+		"unicorn/prevent-abbreviations": "off", // code is sometimes clearer with abbreviations
+		"no-else-return": "off", // code is sometimes clearer with an else
+		"no-lonely-if": "off", // code is sometimes clearer with a lonely if
+		"import/extensions": [
+			"error",
 			{
-				js: 'never',
-				ts: 'never',
+				js: "never",
+				ts: "never",
+			},
+		],
+		"@typescript-eslint/naming-convention": [
+			{
+				selector: "default",
+				format: ["camelCase"],
+				leadingUnderscore: "allow",
+				trailingUnderscore: "allow",
+			},
+
+			{
+				selector: "variable",
+				format: ["camelCase", "UPPER_CASE"],
+				leadingUnderscore: "allow",
+				trailingUnderscore: "allow",
+			},
+
+			{
+				selector: "typeLike",
+				format: ["PascalCase"],
+				leadingUnderscore: "allow", // Sometimes generic type parameters are unused
 			},
 		],
 	},
