b28c7c9741f4a2efc543d12fb63c1e2a22d12d40
Leon Si <contact@leonzalion.com>
2022-01-08 12:08:46 -0500

    allow abbreviations

--- src/eslint/global-rules.ts
+++ src/eslint/global-rules.ts
@@ -3,6 +3,7 @@ module.exports = {
 		'import/no-unassigned-import': 'off',
 		'@typescript-eslint/consistent-type-imports': 'error',
 		'node/file-extensions-in-import': 'off',
+		'unicorn/prevent-abbreviations': 'off',
 		'no-lonely-if': 'off',
 		'import/extensions': [
 			'error',
