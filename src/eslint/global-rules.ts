b4e66886595d352060edc10332e3344133369464
Leon Si <contact@leonzalion.com>
2022-01-12 14:28:59 -0500

    feat: enforce extensions

--- src/eslint/global-rules.ts
+++ src/eslint/global-rules.ts
@@ -2,15 +2,13 @@ module.exports = {
 	rules: {
 		'import/no-unassigned-import': 'off',
 		'@typescript-eslint/consistent-type-imports': 'error',
-		'node/file-extensions-in-import': 'off',
 		'unicorn/prevent-abbreviations': 'off', // code is sometimes clearer with abbreviations
 		'no-else-return': 'off', // code is sometimes clearer with an else
 		'no-lonely-if': 'off', // code is sometimes clearer with a lonely if
 		'import/extensions': [
 			'error',
 			{
-				js: 'never',
-				ts: 'never',
+				js: 'always',
 			},
 		],
 		'@typescript-eslint/naming-convention': [
