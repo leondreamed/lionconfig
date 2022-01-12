43034f348e61375cbc51d1d44fb9de6795169b9f
Leon Si <contact@leonzalion.com>
2022-01-12 14:34:46 -0500

    back to default

--- src/eslint/global-rules.ts
+++ src/eslint/global-rules.ts
@@ -2,14 +2,9 @@ module.exports = {
 	rules: {
 		'import/no-unassigned-import': 'off',
 		'@typescript-eslint/consistent-type-imports': 'error',
-		"node/file-extension-in-import": [
-			"error",
-			"always"
-		],
 		'unicorn/prevent-abbreviations': 'off', // code is sometimes clearer with abbreviations
 		'no-else-return': 'off', // code is sometimes clearer with an else
 		'no-lonely-if': 'off', // code is sometimes clearer with a lonely if
-		'import/extensions': ['error', 'always'],
 		'@typescript-eslint/naming-convention': [
 			'error',
 			{
