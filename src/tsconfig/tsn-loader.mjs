03bc40c8223d74f283bd01abe0100669c06f54c9
Leon Si <contact@leonzalion.com>
2022-01-20 22:54:56 -0500

    fix laoder

--- src/tsconfig/tsn-loader.mjs
+++ src/tsconfig/tsn-loader.mjs
@@ -6,18 +6,7 @@ const { absoluteBaseUrl, paths } = tsConfigPaths.loadConfig();
 const matchPath = tsConfigPaths.createMatchPath(absoluteBaseUrl, paths);
 
 export function resolve(specifier, ctx, defaultResolve) {
-	const lastIndexOfIndex = specifier.lastIndexOf('/index.js');
-	if (lastIndexOfIndex !== -1) {
-		// Handle index.js
-		const trimmed = specifier.substring(0, lastIndexOfIndex);
-		const match = matchPath(trimmed);
-		if (match)
-			return resolveTs(
-				pathToFileURL(`${match}/index.js`).href,
-				ctx,
-				defaultResolve
-			);
-	} else if (specifier.endsWith('.js')) {
+	if (specifier.endsWith('.js')) {
 		// Handle *.js
 		const trimmed = specifier.substring(0, specifier.length - 3);
 		const match = matchPath(trimmed);
