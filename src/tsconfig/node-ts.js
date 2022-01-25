71dc20169285ae7dcc57d46f8b43254245d94560
Leon Si <contact@leonzalion.com>
2022-01-24 19:35:07 -0500

    fix node script

--- src/tsconfig/node-ts.js
+++ src/tsconfig/node-ts.js
@@ -7,7 +7,7 @@ spawnSync(
 		'@leonzalion/configs/tsconfig/suppress-experimental-loader-warning.cjs',
 		'--loader',
 		'@leonzalion/configs/tsconfig/ts-loader.mjs',
-		...process.argv.slice(1),
+		...process.argv.slice(2),
 	],
 	{
 		stdio: 'inherit',
