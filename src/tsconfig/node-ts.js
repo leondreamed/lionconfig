25c6c32848e396e339707af4b80487c04458e884
Leon Si <contact@leonzalion.com>
2022-01-26 14:14:08 -0500

    update configs

--- src/tsconfig/node-ts.js
+++ src/tsconfig/node-ts.js
@@ -1,5 +1,6 @@
 const { spawnSync } = require('child_process');
 const path = require('path');
+const minimist = require('minimist');
 
 // The path of the project
 const projectPath = process.argv[1].slice(
@@ -7,8 +8,15 @@ const projectPath = process.argv[1].slice(
 	process.argv[1].indexOf('/node_modules/')
 );
 
-// The file to run
-const file = process.argv.at(-1);
+// The first CLI argument that doesn't have an option associated with it
+// is the file
+const file = minimist(process.argv)._[0];
+if (file === undefined) {
+	throw new Error('No file specified.');
+}
+
+const fileIndex = process.argv.indexOf(fileIndex);
+const argv = [...process.argv.slice(2, fileIndex), ...process.argv.slice(fileIndex + 1, process.argv.length)]
 
 spawnSync(
 	'node',
@@ -17,11 +25,11 @@ spawnSync(
 		'@leonzalion/configs/tsconfig/suppress-experimental-loader-warning.cjs',
 		'--loader',
 		'@leonzalion/configs/tsconfig/ts-loader.mjs',
-		...process.argv.slice(2, -1),
+		...argv,
 		path.join(projectPath, file),
 	],
 	{
 		stdio: 'inherit',
-		cwd: path.join(projectPath, path.dirname(process.argv[2])),
+		cwd: path.join(projectPath, path.dirname(file)),
 	}
 );
