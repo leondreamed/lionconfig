026e9f35a320fb336d104bb1822d88e185aa83f3
Leon Si <contact@leonzalion.com>
2022-01-26 12:20:41 -0500

    update node script

--- src/tsconfig/node-ts.js
+++ src/tsconfig/node-ts.js
@@ -1,4 +1,14 @@
 const { spawnSync } = require('child_process');
+const path = require('path');
+
+// The path of the project
+const projectPath = process.argv[1].slice(
+	0,
+	process.argv[1].indexOf('/node_modules/')
+);
+
+// The file to run
+const file = process.argv.at(-1);
 
 spawnSync(
 	'node',
@@ -7,9 +17,11 @@ spawnSync(
 		'@leonzalion/configs/tsconfig/suppress-experimental-loader-warning.cjs',
 		'--loader',
 		'@leonzalion/configs/tsconfig/ts-loader.mjs',
-		...process.argv.slice(2),
+		...process.argv.slice(2, -1),
+		path.join(projectPath, file),
 	],
 	{
 		stdio: 'inherit',
+		cwd: path.join(projectPath, path.dirname(process.argv[2])),
 	}
 );
