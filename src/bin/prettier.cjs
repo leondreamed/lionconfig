e03bb39afb16ea0de5338338beb624eef27deef6
Leon Si <contact@leonzalion.com>
2022-04-04 21:43:01 -0400

    feat: prettierignore

--- src/bin/prettier.cjs
+++ src/bin/prettier.cjs
@@ -1,6 +1,9 @@
+const path = require('path');
 const { executeBin } = require('../utils/bin.cjs');
 
 executeBin('prettier', 'bin-prettier.js', [
 	'--config',
-	require.resolve('../prettier.cjs')
+	require.resolve('../prettier.cjs'),
+	'--ignore-path',
+	path.join(__dirname, '../prettier/.prettierignore')
 ]);
