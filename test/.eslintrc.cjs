3dfd3723b234480f92d5e257c84fbf005409fc20
Leon Si <contact@leonzalion.com>
2022-04-19 00:02:05 -0400

    chore: update lion-system

--- test/.eslintrc.cjs
+++ test/.eslintrc.cjs
@@ -1,8 +1,8 @@
 const path = require('path');
 
 module.exports = {
-	extends: ['../.eslintrc.cjs'],
+	extends: '../.eslintrc.cjs',
 	parserOptions: {
-		project: [path.resolve(__dirname, './tsconfig.eslint.json')],
+		project: path.resolve(__dirname, './tsconfig.eslint.json'),
 	},
 };
