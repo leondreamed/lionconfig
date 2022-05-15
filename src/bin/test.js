e9f51fcfb582af5c141dbb59cd8fa35e778ca0f2
Leon Si <contact@leonzalion.com>
2022-05-15 07:18:06 -0400

    fix: condition for test

--- src/bin/test.js
+++ src/bin/test.js
@@ -1,7 +1,21 @@
 #!/usr/bin/env node
 
+import * as fs from 'node:fs';
+import * as path from 'node:path';
 import process from 'node:process';
 
 import { runScript } from '../utils/script.js';
 
-runScript(['vitest', 'run', ...process.argv.slice(2)]);
+runScript(['vitest', 'run', ...process.argv.slice(2)], (dir) => {
+	const vitestFiles = [
+		'vite.config.ts',
+		'vite.config.js',
+		'vitest.config.js',
+		'vitest.config.ts',
+	];
+
+	return (
+		fs.existsSync(path.join(dir, 'test')) ||
+		vitestFiles.some((file) => fs.existsSync(path.join(dir, file)))
+	);
+});
