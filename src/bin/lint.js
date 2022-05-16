dba3707cbec9033869a2d858a997662870784cec
Leon Si <contact@leonzalion.com>
2022-05-16 15:17:11 -0400

    feat: smarter lint script

--- src/bin/lint.js
+++ src/bin/lint.js
@@ -4,4 +4,6 @@ import process from 'node:process';
 
 import { runScript } from '../utils/script.js';
 
-runScript('lint', ['eslint', '--cache', '--fix', ...process.argv.slice(2), '.']);
+runScript('lint', ['eslint', '--cache', '--fix', ...process.argv.slice(2), '.'], (dir) =>
+	fs.existsSync(path.join(dir, '.eslintrc.cjs'))
+);
