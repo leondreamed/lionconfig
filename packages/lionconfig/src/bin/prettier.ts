943d0caf11b611e7542cdad8d14b1c385749a115
Leon Si <contact@leonzalion.com>
2022-05-17 13:21:48 -0400

    fix: binaries

--- packages/lionconfig/src/bin/prettier.ts
+++ packages/lionconfig/src/bin/prettier.ts
@@ -1,9 +1,64 @@
 #!/usr/bin/env node
 
 import { join } from 'desm';
-import { execaSync } from 'execa';
+import fs from 'node:fs';
+import path from 'node:path';
 import process from 'node:process';
+import { outdent } from 'outdent';
+import pkgUp from 'pkg-up';
 import resolve from 'resolve';
+
+async function prettierWrapper() {
+	const defaultPrettierIgnoreFilePath = join(
+		import.meta.url,
+		'.prettierignore'
+	);
+	const projectDir = path.dirname(pkgUp.sync()!);
+
+	// Wrapping around prettier in order to support extended `.prettierignore` files
+
+	const readFileAsync = fs.promises.readFile;
+
+	// prettier uses `fs.promises` to read files: https://github.com/prettier/prettier/blob/main/src/utils/get-file-content-or-null.js
+
+	fs.promises.readFile = (async (filename, encoding) => {
+		if (filename === defaultPrettierIgnoreFilePath) {
+			let defaultPrettierIgnore = await readFileAsync(filename, encoding);
+
+			// This is ugly but it's the only way to prevent Prettier from ignoring the `temp` folder during
+			// testing
+			if (process.env.VITEST) {
+				defaultPrettierIgnore = String(defaultPrettierIgnore).replace(
+					/^temp$/m,
+					''
+				);
+			}
+
+			const projectPrettierIgnorePath = path.join(
+				projectDir,
+				'.prettierignore'
+			);
+			// Check if the user has a .prettierignore in their project
+			if (fs.existsSync(projectPrettierIgnorePath)) {
+				const projectPrettierIgnore = await readFileAsync(
+					projectPrettierIgnorePath,
+					'utf8'
+				);
+				return outdent`
+					${defaultPrettierIgnore}
+					${projectPrettierIgnore}
+				`;
+			} else {
+				return defaultPrettierIgnore;
+			}
+		} else {
+			return readFileAsync(filename, encoding);
+		}
+	}) as typeof fs.promises.readFile;
+
+	// @ts-expect-error: Prettier doesn't export the type of their `bin-prettier.js` file
+	await import('prettier/bin-prettier.js');
+}
 
 const argv = process.argv.slice(2);
 
@@ -14,10 +69,6 @@ if (customConfigIndex !== -1) {
 }
 
 const ignorePath = join(import.meta.url, '../prettier/.prettierignore');
-const prettierWrapperBinPath = join(
-	import.meta.url,
-	'../prettier/wrapper-bin.js'
-);
 
 const prettierOptions = [`--ignore-path=${ignorePath}`];
 
@@ -27,8 +78,6 @@ if (customConfigIndex !== -1) {
 
 prettierOptions.push(...argv);
 
-process.exit(
-	execaSync(prettierWrapperBinPath, prettierOptions, {
-		stdio: 'inherit',
-	}).exitCode
-);
+// Overriding argv before importing the Prettier binary
+process.argv = [...process.argv.slice(0, 2), ...argv];
+await prettierWrapper();
