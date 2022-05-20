9691a0c34717795f3cdeeb0121b592332be1b55b
Leon Si <contact@leonzalion.com>
2022-05-20 19:03:10 -0400

    refactor: re-export from lion-utils

--- packages/lionconfig/src/index.ts
+++ packages/lionconfig/src/index.ts
@@ -1,10 +1,9 @@
 export { copyPackageFiles, packageFiles } from './utils/copy.js';
 export { commitMsg, preCommit, prePush } from './utils/hooks.js';
-export { updateJsonFile } from './utils/json.js';
 export {
 	removePreinstallScript,
 	rewritePackageJsonPaths,
 	transformPackageJson,
 } from './utils/package-json.js';
-export { chProjectDir, getProjectDir } from './utils/project-dir.js';
 export { rmDist } from './utils/rm.js';
+export { chProjectDir, getProjectDir, updateJsonFile } from 'lion-utils';
