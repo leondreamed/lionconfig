e3fc691e0795bac192c605174fb0fe7158799224
Leon Si <contact@leonzalion.com>
2022-05-28 08:28:52 -0400

    fix: remove tsc-check

--- packages/lionconfig/src/bin/typecheck.ts
+++ packages/lionconfig/src/bin/typecheck.ts
@@ -3,29 +3,8 @@
 import * as fs from 'node:fs';
 import * as path from 'node:path';
 import process from 'node:process';
-import { loadSync as loadTsconfigSync } from 'tsconfig';
 
 import { runScript } from '../utils/script.js';
-
-// Run the root-level special tsc-check if the root `tsconfig.json` uses references.
-if (fs.existsSync('tsconfig.json')) {
-	const { config: tsconfigJson } = loadTsconfigSync('tsconfig.json') as {
-		config: {
-			files?: string[];
-			references?: string[];
-		};
-	};
-
-	if (
-		tsconfigJson.files !== undefined &&
-		tsconfigJson.files.length === 0 &&
-		tsconfigJson.references !== undefined &&
-		tsconfigJson.references.length > 0
-	) {
-		await import('./tsc-check.js');
-		process.exit(0);
-	}
-}
 
 // Otherwise, run a recursive `typecheck` script
 await runScript({
