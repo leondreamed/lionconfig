c062cec25cc925bd3cdf9cf50a9994618090328d
Leon Si <leon@leondreamed.com>
2022-12-13 18:01:40 -0500

    fix: paths

--- packages/lionconfig/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { copyPackageFiles, packageFiles } from './utils/copy.js';
-export { commitMsg, preCommit, prePush } from './utils/hooks.js';
-export { removePreinstallScript, rewritePackageJsonPaths, transformPackageJson, } from './utils/package-json.js';
-export { rmDist } from './utils/rm.js';
-export { chProjectDir, getProjectDir, updateJsonFile } from 'lion-utils';
