622699ecec048266feacfa7fc1551c4743e0f6f7
Leon Si <leon@leondreamed.com>
2022-12-26 09:34:11 -0500

    fix: use tsc path

--- packages/lionconfig/src/utils/tsc.ts
+++ packages/lionconfig/src/utils/tsc.ts
@@ -1,25 +1,35 @@
+import { createRequire } from 'node:module'
+import path from 'node:path'
+
 import { execa } from 'execa'
 import { replaceTscAliasPaths } from 'tsc-alias'
 
 /**
-	For some reason, setting `declarationDir` in `tsconfig.json` to the same value as `outDir` breaks `tsc`.
+	For some reason, setting `declarationDir` to the same value as `outDir` in `tsconfig.json` breaks `tsc`.
 
 	Thus, we pass `declarationDir` manually.
 */
 export async function tsc(options?: { tsConfigPath?: string }) {
+	const tscPath = path.join(
+		createRequire(process.cwd()).resolve('typescript/package.json'),
+		'bin/tsc'
+	)
+
 	if (options?.tsConfigPath === undefined) {
-		await execa('pnpm', ['exec', 'tsc'], { stdio: 'inherit' })
-		await execa('pnpm', ['exec', 'tsc', '--emitDeclarationOnly', '--declarationDir', 'dist'], {
-			stdio: 'inherit',
-		})
+		await execa(tscPath, { stdio: 'inherit' })
+		await execa(
+			tscPath,
+			['--emitDeclarationOnly', '--declarationDir', 'dist'],
+			{
+				stdio: 'inherit',
+			}
+		)
 		await replaceTscAliasPaths({ declarationDir: 'dist' })
 	} else {
-		await execa('pnpm', ['exec', 'tsc', '-p', options.tsConfigPath], { stdio: 'inherit' })
+		await execa(tscPath, ['-p', options.tsConfigPath], { stdio: 'inherit' })
 		await execa(
-			'pnpm',
+			tscPath,
 			[
-				'exec',
-				'tsc',
 				'-p',
 				options.tsConfigPath,
 				'--emitDeclarationOnly',
