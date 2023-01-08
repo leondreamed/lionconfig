695db9fb06fd45895c37286fd3f55320c4859bd9
Leon Si <leon@leondreamed.com>
2023-01-08 16:26:24 -0500

    fix: tsconfig

--- packages/lionconfig/scripts/build.ts
+++ packages/lionconfig/scripts/build.ts
@@ -1,3 +1,5 @@
+import fs from 'node:fs'
+
 import { chmodrSync } from 'chmodrp'
 import { join } from 'desm'
 
@@ -6,12 +8,8 @@ import { copyPackageFiles, rmDist, tsc } from '../src/index.js'
 rmDist()
 await tsc({ tsConfigPath: join(import.meta.url, '../tsconfig.json') })
 await copyPackageFiles({
-	additionalFiles: [
-		'src/git',
-		'src/tsconfig.json',
-		'src/markdownlint.json',
-		'src/bin/ts-node.sh',
-	],
+	additionalFiles: ['src/git', 'src/markdownlint.json', 'src/bin/ts-node.sh'],
 })
+fs.copyFileSync('src/base.json', 'dist/tsconfig.json')
 
 chmodrSync('dist/bin', 0o755)
